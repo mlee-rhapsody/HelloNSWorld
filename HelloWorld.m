4f2a1171477d0ea1009df7d785c0f7a31b913e22
Miene Lee <mlee@rhapsody.com>
2011-04-24 22:56:11 -0700

    my helloworld code

---
+++ HelloWorld.m
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+	NSLog(@"Hello World");
+
+	[pool drain];
+	return 0;
+}
